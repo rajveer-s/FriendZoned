934eef8e56dbe1e21b9dece42a7ca6e91d654246
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-25 00:30:18 -0700

    controllers/user POST user added

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,4 +14,11 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
+
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
 }
