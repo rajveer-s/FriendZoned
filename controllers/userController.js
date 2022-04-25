fbebbc46e80afbe9b001ebc9b4be9336af3f9f81
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-25 00:29:54 -0700

    controllers/user GET all users added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,4 +2,16 @@ const { User, Thought } = require('../models');
 
 module.exports = {
 
+  // Get all Users
+  getUser(req, res) {
+    User.find()
+      .populate('friends')
+      .then((users) => {
+        return res.json(users);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
 }
