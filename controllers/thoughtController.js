22eabd83ab4f6195222fabee0dbe5db823d32eb4
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-27 21:31:30 -0700

    get thoughts by ID route created

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,4 +9,19 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
+  getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.id })
+      .populate('reactions')
+      .select('-__v')
+      .then((thoughts) =>
+        !thoughts
+          ? res.status(404).json({ message: 'No User with that ID' })
+          : res.json({ thoughts })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
 }
